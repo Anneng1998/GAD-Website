0b4c6cf473e85fdd053573a79d068a12f4a99cec
Anneng1998 <rhoanne.cera@gmail.com>
2023-04-14 11:28:29 +0800

    first comment

--- database/db.php
+++ database/db.php
@@ -4,6 +4,12 @@
 
 // $db = mysqli_connect('localhost','root','','db_gad_rms');
 
-$db = mysqli_connect('localhost','u774533397_db_gad_rms','P@ss123456','db_u774533397_db_gad_rms');
+$db = mysqli_connect('localhost','u774533397_db_gad_rms','@User2022','u774533397_db_gad_rms');
+
+// if ($db) {
+//     echo 'good';
+// } else {
+//     echo 'bad';
+// }
 
 ?>
