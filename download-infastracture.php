513d34ef1d38fe5e1c45a1b6329e4b43107f8da8
Anneng1998 <rhoanne.cera@gmail.com>
2023-05-03 17:11:34 +0800

    comment

--- download-infastracture.php
+++ download-infastracture.php
@@ -7,36 +7,36 @@
     $filename_sql = mysqli_query($db, "SELECT * FROM tbl_infactracture WHERE fldID = '$id'");
     $filename_fetch = mysqli_fetch_array($filename_sql);
 
-    echo $filename_1 = "file/infastracture/".$filename_fetch['fldHGDG'];
-    echo $filename_2 = "file/infastracture/".$filename_fetch['fldCopyOfProposal'];
-    // $title = $filename_fetch['fldTitle'];
+    $filename_1 = "files/infastracture/".$filename_fetch['fldHGDG'];
+    $filename_2 = "files/infastracture/".$filename_fetch['fldCopyOfProposal'];
+    $title = $filename_fetch['fldTitle'];
 
-    // $files = array($filename_1, $filename_2);
+    $files = array($filename_1, $filename_2);
 
-    // # create new zip opbject
-    // $zip = new ZipArchive();
+    # create new zip opbject
+    $zip = new ZipArchive();
 
-    // # create a temp file & open it
-    // $tmp_file = tempnam('.','');
-    // $zip->open($tmp_file, ZipArchive::CREATE);
+    # create a temp file & open it
+    $tmp_file = tempnam('.','');
+    $zip->open($tmp_file, ZipArchive::CREATE);
 
-    // # loop through each file
-    // foreach($files as $file){
+    # loop through each file
+    foreach($files as $file){
 
-    //     # download file
-    //     $download_file = file_get_contents($file);
+        # download file
+        $download_file = file_get_contents($file);
 
-    //     #add it to the zip
-    //     $zip->addFromString(basename($file),$download_file);
+        #add it to the zip
+        $zip->addFromString(basename($file),$download_file);
 
-    // }
+    }
 
-    // # close zip
-    // $zip->close();
+    # close zip
+    $zip->close();
 
-    // # send the file to the browser as a download
-    // header('Content-disposition: attachment; filename='.$title.'.zip');
-    // header('Content-type: application/zip');
-    // readfile($tmp_file);
+    # send the file to the browser as a download
+    header('Content-disposition: attachment; filename='.$title.'.zip');
+    header('Content-type: application/zip');
+    readfile($tmp_file);
 
-    // // gagana kaya hahaha
+    // gagana kaya hahaha
