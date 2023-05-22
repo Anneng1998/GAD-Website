768fe0c41246d23e0a1dba27f1a6f88bc5be40ec
Anneng1998 <rhoanne.cera@gmail.com>
2023-05-22 17:57:04 +0800

    comment

--- news.php
+++ news.php
@@ -17,12 +17,61 @@
 <div class="card d-block" style="box-shadow: 1px 2px 5px #333;">
 
  <!-- start page title -->
- <div class="row">
-    <div class="col-12">
-        <div class="page-title-box">
-            <div class="page-title-right">
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#primary-header-modal">Add New</button>
+                <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#primary-header-modal">Add Events</button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#primary-header-modal">View News Information</button>
+                <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#primary-header-modal">View Events Information</button>    
             </div>
-            <h4 class="page-title">News and Updates</h4>
+                <h4 class="page-title">News and Updates</h4>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-6">
+        <div class="card d-block" style="box-shadow: 1px 2px 5px #333;"> <br>
+            <h2 style="padding-left: 2%;">News</h2>
+
+            <div class="card d-block" style="box-shadow: 1px 2px 5px #333; margin: 20px;">
+
+                <div class="card">
+                    <div class="card-header">
+                        News title
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">time and date</h5>
+                        <p class="card-text">Description</p>
+                    </div>
+                </div>
+            
+            </div><br>
+
+        </div>
+    </div>
+
+    <div class="col-6">
+        <div class="card d-block" style="box-shadow: 1px 2px 5px #333;"><br>
+            <h2 style="padding-left: 2%;">Events</h2>
+
+            <div class="card d-block" style="box-shadow: 1px 2px 5px #333; margin: 20px;">
+
+                <div class="card">
+                    <div class="card-header">
+                        Events title
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">time and date</h5>
+                        <p class="card-text">Description</p>
+                    </div>
+                </div>
+            
+            </div><br>
+
         </div>
     </div>
 </div>
