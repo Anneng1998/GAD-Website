46904bff692ee1580742e05ab5dd445dd57dfd05
Anneng1998 <rhoanne.cera@gmail.com>
2023-05-22 10:44:18 +0800

    comment

--- stakeholder/student.php
+++ stakeholder/student.php
@@ -42,7 +42,7 @@ if (isset($_POST['save'])){
                             email) 
                         VALUES (
                             '$UniqueID', 
-                            'RDE',
+                            'STUDENT',
                             '$fname', 
                             '$mname', 
                             '$lname', 
