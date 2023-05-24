59edeea20befd0378f9bb5a810a44b78ffda091b
Anneng1998 <rhoanne.cera@gmail.com>
2023-05-24 09:53:33 +0800

    comment

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ include "database/db.php";
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-1" ></div>
-			<div class="col-md-4" >
+			<div class="col-md-5" >
 				
 				<h3>News</h3>
 					<hr>
@@ -98,7 +98,7 @@ include "database/db.php";
 				}
 				?>
 			</div>
-			<div class="col-md-2" >
+			<div class="col-md-1" >
 			</div>
 			<div class="col-md-4">
 				<h3>Programs and Events</h3>
